5f9f0698afa5a05359ee4d623994afa620410d7f
Shikhar Arora <shikhararora98@gmail.com>
2018-12-27 17:57:02 +0000

    First Commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/shikhar/Desktop/Machine-Learning-Web-Apps-f7540130cf1ce378310d9d70ff3e59d707d785ed/Machine-Learning-Web-Apps-f7540130cf1ce378310d9d70ff3e59d707d785ed/Youtube-Spam-Detector-ML-Flask-App/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
